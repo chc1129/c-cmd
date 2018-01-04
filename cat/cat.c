12d5303596b606cc2b1f449d8ea230737213fce5
chc1129 <chc1129@gmail.com>
2018-01-04 21:46:06 +0000

    cat cmd commit

---
+++ cat/cat.c
@@ -0,0 +1,21 @@
+#include <sys/param.h>
+#include <sys/stat.h>
+
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+  int ch;
+  struct flock stdout_lock;
+
+  setprogram(argv[0]);
+
+}
+
