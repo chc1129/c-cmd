93d9d43052ab4f9ae4cf41546141994cb5cd31d4
chc1129 <chc1129@gmail.com>
2018-01-06 08:50:20 +0000

    update cat.c

--- cat/cat.c
+++ cat/cat.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[]) {
   int ch;
   struct flock stdout_lock;
 
-  setprogram(argv[0]);
-printf ("argv[0] = %s", argv[0]);
+  setprogram( argv[0] );
+printf ( "argv[0] = %s", argv[0] );
   // 全てのロケールを環境変数依存に設定
-  ( void )setlocale( LC_ALL, "");
+  ( void )setlocale( LC_ALL, "" );
 
   // getoptでコマンドオプションを判定
   while (( ch = getopt(argc, argv, "B:beflnstuv" )) != -1) {
@@ -63,13 +63,13 @@ printf ("argv[0] = %s", argv[0]);
       // default と ?の位置は逆がセオリーでは?
       // ってかコレなら'?'は明記しなくても同一ルート通るのだが
       // 以下L66-L68は別関数にする方が見やすい
-          ( void )fprintf( stderr, "Usage: %s [-beflnstuv] [-B bsize] [-] " "[file ...]\n",
-                           getprogname() );
+          ( void )fprintf( stderr, "Usage: %s [-beflnstuv] [-B bsize] [-] " "[file ...]\n", getprogname() );
           return EXIT_FAILURE;
       }
 // optindは外部変数
 printf("optind: %d\n", optind);
-  argv += optind;
+      argv += optind;
+  }
 
 // if (a) という様な書き方好きになれないテクがないだけなんだろうけど
 // if ( a != FALSE ) とかにして欲しい
@@ -83,16 +83,113 @@ printf("optind: %d\n", optind);
       }
     }
 
-    if (bflag || eflag || nflag || sflag || tflag || vflag ) {
+    if ( bflag || eflag || nflag || sflag || tflag || vflag ) {
         cook_args( argv );
     } else {
         raw_args( argv );
     }
-    if (fclose(stdout)) {
+    if ( fclose( stdout ) ) {
           err( EXIT_FAILURE, "stdout" );
     }
 
-  }
   return rval;
 }
+
+void cook_args( char **argv ) {
+  FILE *fp;
+
+  fp = stdin;
+  filename = "stdin";
+  do {
+      if ( *argv ) {
+        if ( !strcmp( *argv, "-" ) ) {
+          fp = stdin;
+        } else if (( fp = fopen( * argv, fflag ? "rf" : "r")) == NULL ) {
+          warn( "%s", *argv );
+          rval = EXIT_FAILURE;
+          ++argv;
+          continue;
+        }
+        filename = *argv++;
+      }
+      cook_buf( fp );
+      if ( fp != stdin ) {
+        ( void )fclose( fp );
+      } else {
+        clearerr( fp );
+      }
+  } while ( *argv );
+}
+
+void cook_buf( FILE *fp ) {
+  int ch, gobble, line, prev;
+
+  line = gobble = 0;
+  for ( prev = '\n'; ( ch = getc( fp ) ) != EOF; prev = ch ) {
+    if ( prev == '\n' ) {
+      if ( sflag ) {
+        if ( ch == '\n' ) {
+          if ( gobble ) {
+            continue;
+          }
+          gobble = 1;
+        } else {
+          gobble = 0;
+        }
+        if ( nflag ) {
+          if ( !bflag || ch != '\n' ) {
+            ( void )fprintf( stdout, "%6d\t", ++line );
+            if ( ferror( stdout ) ) {
+              break;
+            }
+          } else if ( eflag ) {
+            ( void )fprintf( stdout, "%6s\t", "" );
+            if ( ferror( stdout ) ) {
+              break;
+            }
+          }
+        }
+        if ( ch == '\n' ) {
+          if ( eflag ) {
+            if ( putchar( '$' ) == EOF ) {
+              break;
+            }
+          }
+        } else if ( ch == '\t' ) {
+          if ( tflag ) {
+            if ( putchar( '^' ) == EOF || putchar( 'I' ) == EOF ) {
+              break;
+            }
+            continue;
+          }
+        } else if ( vflag ) {
+          if ( !isascii( ch ) ) {
+            if ( putchar( 'M' ) == EOF || putchar( '-' ) == EOF ) {
+              break;
+            }
+            ch = toascii( ch );
+          }
+          if ( iscntrl( ch )) {
+            if ( putchar( '^' ) == EOF || putchar( ch == '177' ? '?' : ch | 0100 ) == EOF ) {
+              break;
+            }
+            continue;
+          }
+        }
+        if ( putchar( ch ) == EOF ) {
+          break;
+        }
+      }
+      if ( ferror( fp ) ) {
+        warn( "%s", filename );
+        rval = EXIT_FAILURE;
+        clearerr( fp );
+      }
+      if ( ferror( stdout )) {
+        err( EXIT_FAILURE, "stdout" ) ;
+      }
+    }
+  }
+}
+
 
