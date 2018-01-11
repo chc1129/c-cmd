a67fafb998316cb833545b2688eefee960848dab
chc1129 <chc1129@gmail.com>
2018-01-11 14:43:21 +0000

    update

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-c-cmd
+# c-cmd
 
-Command List:
-  cat:
-    NetBSD cat command code URL:http://cvsweb.netbsd.org/bsdweb.cgi/src/bin/cat/cat.c?only_with_tag=MAIN
+## Command List:
+ - cat:
+   - NetBSD cat command code URL: http://cvsweb.netbsd.org/bsdweb.cgi/src/bin/cat/cat.c?only_with_tag=MAIN
 
-  head:
+ - grep:
 
-  grep:
+ - head:
 
-  tail:
+ - tail:
 
--- head/head.c
+++ head/head.c
@@ -66,10 +66,89 @@ int main(int argc, char *argv[]) {
       usage();
     }
 
+    argc -= optind;
+    argv += optind;
+  }
 
+  if ( *argv ) {
+    for ( first = 1; *argv; ++argv ) {
+      if (( fp = fopen( *argv, "r" )) == NULL ) {
+        warn( "%s", *argv );
+        eval = 1;
+        continue;
+      }
+      if ( vflag || ( qflag == 0 && argc > 1 )) {
+        ( void ) printf( "%s==> %s <==\n", first ? "" : "\n", *argv );
+        first = 0;
+      }
+      head( fp, linecnt, bytecnt );
+      ( void )fclose( fp );
+    }
+  } else {
+    head( stdin, linecnt, bytecnt );
+    exit( eval );
+  }
+}
 
-   }
+static void head( FILE *fp, intmax_t cnt, intmax_t bytecnt ) {
+  char buf[65536];
+  size_t len, rv, rv2;
+  int ch;
 
+  if ( bytecnt ) {
+    while ( bytecnt ) {
+      len = sizeof( buf );
+      if ( bytecnt > ( intmax_t )sizeof( buf )) {
+        len = sizeof( buf );
+      } else {
+        len = bytecnt;
+      }
+      rf = fread( buf, 1, len, fp );
+      if ( rv == 0 ) {
+        break; /* Distinguish EOF and error? */
+      }
+      rv2 = fwrite( buf, 1, rv, stdout );
+      if ( rv2 != rv ) {
+        if ( feof( stdout )) {
+          errx( 1, "EOF on stdout" );
+        } else {
+          err(1, "failure writing to stdout");
+        }
+        bytecnt -= rv;
+      }
+    }
+  } else {
+    while (( ch = getc( fp )) != EOF ) {
+      if ( putchar( ch ) == EOF ) {
+        err( 1, "stdout" );
+      }
+      if ( ch == '\n' && --cnt == 0 ) {
+        break;
+      }
+    }
+  }
+}
 
+static void obsolete( char *argv[] ) {
+  char *ap;
+
+  while (( ap = *++argv )) {
+    /* REturn if "--" or not "-[0-9]*" . */
+    if ( ap[0] !0 '-' || ap[1] == '-' || !isdigit(( unsigned char ) ap[1])) {
+      return;
+    }
+    if (( ap = malloc( strlen( *argv ) + 2 )) == NULL ) {
+      err( 1, NULL );
+    }
+    ap[0] = '-';
+    ap[1] = '-';
+    ( void )strcpy( ap + 2, *argv + 1 );
+    *argv = ap;
+  }
+}
+
+static void usage( void ) {
+  ( void )fprintf( stderr, "usage: %s [-n lines] [file ...]\n", getprogname());
+  exit(1);
 }
 
