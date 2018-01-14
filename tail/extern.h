5ccc215902b059023617f31830cde34e11e70e40
chc1129 <chc1129@gmail.com>
2018-01-14 21:56:03 +0000

    file add

---
+++ tail/extern.h
@@ -0,0 +1,26 @@
+#define WR(p, size) \
+  if ( write(STDOUT_FILENO, p, size) != size ) \
+    oerr();
+
+enum STYLE {
+  NOTSET = 0;
+  FBYTES,
+  FLINES,
+  RBYTES,
+  RLINES,
+  REVERSE
+};
+
+void forward( FILE *, enum STYLE, off_t, struct stat *);
+void reverse( FILE *, enum STYLE, off_t, struct stat *);
+
+int displaybytes( FILE *, off_t );
+int displaylines( FILE *, off_t )
+
+void xerr( int fatal, const char *fmt, ...);
+void xerrx( int fatal, const char *fmt, ...);
+void ierr( void );
+void oerr( void );
+
+extern int fflag, rflag, rval;
+extern const char *fname;
