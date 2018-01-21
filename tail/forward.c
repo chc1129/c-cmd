d1f60e8864b880410c5dc66359ad84eb7aa39151
chc1129 <chc1129@gmail.com>
2018-01-21 16:03:38 +0000

    file add

---
+++ tail/forward.c
@@ -0,0 +1,17 @@
+#include <sys/cdefs.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/event.h>
+
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "extern.h"
+
