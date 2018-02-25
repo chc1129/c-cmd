fbeb06d0562af6ad9f83516af83d04233257e9ff
chc1129 <chc1129@gmail.com>
2018-02-25 15:17:59 +0000

    update

--- grep/grep.c
+++ grep/grep.c
@@ -7,4 +7,36 @@
 #include <err.h>
 #include <errno.h>
 #include <getopt.h>
+#include <limits.h>
+#include <libgen.h>
+#include <local.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "grep.h"
+
+/*
+ * Default message to use when NLS is disabled or no catalogue
+ * is found.
+ */
+const char *errstr[] = {
+        "",
+/* 1*/  "(standard input)",
+/* 2*/  "cannot read bzip2 compressed file",
+/* 3*/  "unknown %s option",
+/* 4*/  "usgae: %s [-abcDEFGHhIiJLlmnOoPqRSsUVvwxZz] [-A num] [-B num] [-C[num]]\n",
+/* 5*/  "\t[-e pattern] [-f file] [--binnary-files=value] [--color=when]\n",
+/* 6*/  "\t[--context[-num]] [--directories=action] [--labael] [--line-buffered]\n",
+/* 7*/  "\t[pattern] [file ...]\n",
+/* 8*/  "Binary file %s matches\n",
+/* 9*/  "%s (BSD grep) %s\n",
+};
+
+/* Flags passed to regcomp() and regexec() */
+int              cflags = 0;
+int              eflags = REG_STARTEND;
+
 
