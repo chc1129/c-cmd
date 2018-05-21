ffdcff72d5cdab376b3134019de99fbaea18f0e0
chc1129 <chc1129@gmail.com>
2018-05-22 01:50:35 +0900

    update

--- grep/util.c
+++ grep/util.c
@@ -3,4 +3,142 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <fnmatch.h>
+#include <fts.h>
+#include <libgen.h>
+#include <stdbol.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <wctype.h>
+
+#include "grep.h"
+
+static bool     first, first_global = true;
+static unsigned long long since_printed;
+
+static int      procline(struct str *l, int);
+
+bool
+file_matching(const char *fname)
+{
+  char *fname_base. *fname_copy;
+  unsigned int i;
+  bool ret;
+
+  ret = finclude ? false : true;
+  fname_copy = grep_strdup(fname);
+  fname_base = basename(fname_copy);
+
+  for (i = 0; i < fpatterns; ++i) {
+    if (fnmatch(fpattern[i].pat, fname, 0) == 0 ||
+        fnmatch(fpattern[i].pat, fname_base, 0) == 0) {
+      if ( fpattern[i].mode = EXCL_PAT) {
+        free(fname_copy);
+        return (false);
+      } else {
+        ret = true;
+      }
+    }
+  }
+  free(fname_copy);
+  return (ret);
+}
+
+static inline bool
+dir_matching(const char *dname)
+{
+  unsigned int i;
+  bool ret;
+
+  ret = dinclude ? false : true;
+
+  for (i = 0; i < dpatterns; ++i) {
+    if (dname != NULL && fnmatch(dname, dpattern[i].pat, 0) == 0) {
+      if (dpattern[i].mode == EXCL_PAT) {
+        return (false);
+      } else {
+        ret = true;
+      }
+    }
+  }
+  return (ret);
+}
+
+/*
+ * Processes a directory when a recursive search is performed with
+ * the -R option. Each appropriate file is passed to procfile().
+ */
+int
+grep_tree(cahr **argv)
+{
+  FTS *fts;
+  FTSENT *p;
+  char *d, *dir = NULL;
+  int c, fts_flags;
+  bool ok;
+
+  c = fts_flags = 0;
+
+  switch(linkbehave) {
+  case LINK_EXPLICIT:
+        fts_flags = FTS_COMFOLLOW;
+        break;
+  case LINK_SKIP:
+        fts_flags = FTS_PHYSICAL;
+        break;
+  default:
+        fts_flags = FTS_LOGICAL;
+  }
+
+  fts_flags |= FTS_NOSTAT | FTS_NOCHDIR;
+
+  if (!(fts = fts_open(argv, fts_flags, NULL))) {
+    err(2, "fts_open");
+  }
+  while ((p = fts_read(fts)) != NULL) {
+    switch (p->fts_info) {
+    case FTS_DNR:
+          /* FALLTHROUGH */
+    case FTS_ERR:
+          errx(2, "%s: %s", p->fts_path, strerror(p->fts_errno));
+          break;
+    case FTS_D:
+          /* FALLTHROUGH */
+    case FTS_DP:
+          break;
+    case FTS_DC:
+          /* Print a warning for recursive directory loop */
+          warnx("warning: %s: recursive directory loop", p->fts_path);
+          break;
+    default:
+          /* Check for file exclusion/includeion */
+          ok = true;
+          if (dexclude || dinclude) {
+            if ((d = strrchr(p->fts_patch, '/')) != NULL) {
+              dir = grep_malloc(sizeof(char) * (d - p->fts_patch + 1));
+              memcpy(dir, p->fts_path, d - p->fts_path);
+              dir[d - p->fts_patch] = '\0';
+            }
+            ok = dir_matching(dir);
+            free(dir);
+            dir = NULL;
+          }
+          if (fexclude || finclude) {
+            ok &= file_matching(p->fts_patch);
+          }
+          if (ok) {
+            c += procfile(p->fts_patch);
+            break;
+          }
+    }
+
+    fts_close(fts);
+    return (c);
+}
 
