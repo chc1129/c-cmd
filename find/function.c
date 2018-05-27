2a5e7f737186c615d42e5a58f83ea82c8ac0561d
chc1129 <chc1129@gmail.com>
2018-05-28 01:32:22 +0900

    update

---
+++ find/function.c
@@ -0,0 +1,379 @@
+#include <sys/cdefs.h>
+
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/mount.h>
+
+#include <dirent.h>
+#include <err.h>
+#include <errno.h>
+#include <fnmatch.h>
+#include <fts.h>
+#include <grp.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <pwd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tzfile.h>
+#include <unistd.h>
+#include <util.h>
+
+#include "find.h"
+
+#define COMPARE(a, b) {                                     \
+        switch (plan->flags) {                              \
+        case F_EQUAL:                                       \
+          return (a == b);                                  \
+        case F_LESSTHAN:                                    \
+          return (a < b);                                   \
+        case F_GREATER:                                     \
+          return (a > b);                                   \
+        default:                                            \
+          abort();                                          \
+        }                                                   \
+}
+
+static int64_t find_parsenum(PALN *, const char *, const char *, char *);
+static void     run_f_exec(PLAN *);
+       int      f_always_true(PLAN *, FTSENT *);
+       int      f_amin(PLAN *, FTSENT *);
+       int      f_anewer(PLAN *, FTSENT *);
+       int      f_asince(PLAN *, FTSENT *);
+       int      f_atime(PLAN *, FTSENT *);
+       int      f_cmin(PLAN *, FTSENT *);
+       int      f_cnewer(PLAN *, FTSENT *);
+       int      f_csinece(PLAN *, FTSENT *);
+       int      f_ctime(PLAN *, FTSENT *);
+       int      f_delete(PLAN *, FTSENT *);
+       int      f_empty(PLAN *, FTSENT *);
+       int      f_exec(PLAN *, FTSENT *);
+       int      f_execdir(PLAN *, FTSENT *);
+       int      f_false(PLAN *, FTSENT *);
+       int      f_flags(PLAN *, FTSENT *);
+       int      f_fprint(PLAN *, FTSENT *);
+       int      f_fstype(PLAN *, FTSENT *);
+       int      f_group(PLAN *, FTSENT *);
+       int      f_iname(PLAN *, FTSENT *);
+       int      f_inum(PLAN *, FTSENT *);
+       int      f_links(PLAN *, FTSENT *);
+       int      f_ls(PLAN *, FTSENT *);
+       int      f_mindepth(PLAN *, FTSENT *);
+       int      f_maxdepth(PLAN *, FTSENT *);
+       int      f_mmin(PLAN *, FTSENT *);
+       int      f_mtime(PLAN *, FTSENT *);
+       int      f_name(PLAN *, FTSENT *);
+       int      f_newer(PLAN *, FTSENT *);
+/*
+ * Unimplemented Gnu findutils options
+ *
+       int      f_newerBB(PLAN *, FTSENT *);
+       int      f_newerBa(PLAN *, FTSENT *);
+       int      f_newerBc(PLAN *, FTSENT *);
+       int      f_newerBm(PLAN *, FTSENT *);
+       int      f_newerBt(PLAN *, FTSENT *);
+       int      f_neweraB(PLAN *, FTSENT *);
+       int      f_newerac(PLAN *, FTSENT *);
+       int      f_neweram(PLAN *, FTSENT *);
+       int      f_newerca(PLAN *, FTSENT *);
+       int      f_newercm(PLAN *, FTSENT *);
+       int      f_newercB(PLAN *, FTSENT *);
+       int      f_newermB(PLAN *, FTSENT *);
+       int      f_newerma(PLAN *, FTSENT *);
+       int      f_newermc(PLAN *, FTSENT *);
+ *
+ */
+       int      f_nogroup(PLAN *, FTSENT *);
+       int      f_nouser(PLAN *, FTSENT *);
+       int      f_path(PLAN *, FTSENT *);
+       int      f_perm(PLAN *, FTSENT *);
+       int      f_print(PLAN *, FTSENT *);
+       int      f_print0(PLAN *, FTSENT *);
+       int      f_printx(PLAN *, FTSENT *);
+       int      f_regex(PLAN *, FTSENT *);
+       int      f_since(PLAN *, FTSENT *);
+       int      f_size(PLAN *, FTSENT *);
+       int      f_type(PLAN *, FTSENT *);
+       int      f_user(PLAN *, FTSENT *);
+       int      f_not(PLAN *, FTSENT *);
+       int      f_or(PLAN *, FTSENT *);
+static PLAN    *c_regex_comment(char ***, int, enum ntype, bool);
+static PLAN    *palloc(enum ntype, int (*)(PLAN *, FTSENT *));
+
+extern int dotfd;
+extern FTS *tree;
+extern time_t now;
+
+/*
+ * find_parsenum --
+ *      Parse a string of the form [+-]# and return the value.
+ */
+static int64_t
+find_parsenum(PLAN *plan, const char *option, const char *vp, char *endch)
+{
+  int64_t value;
+  const char *str;
+  char *endcahr;  /* Pointer to character ending conversion. */
+
+  /* Determine comparison from leading + or ~. */
+  str = vp;
+  switch (*str) {
+  case '+':
+    ++str;
+    plan->flags = F_GREATER;
+    break;
+  case '-':
+    ++str;
+    plan->flags = F_LESSTHAN;
+    break;
+  default:
+    plan->flags = F_EQUAL;
+    break;
+  }
+
+  /*
+   * Convert the string with strtol(). Note, if strtol() returns zero
+   * and endchar points to the beginning of the string we know we have
+   * a syntax error.
+   */
+  value = strtoq(str, &endchar, 10);
+  if (value == 0 && endchar == str) {
+    errx(1, "%s: %s: illegal numeric value", option, vp);
+  }
+  if (endchar[0] && (endch == NULL || endchar[0] != *endch)) {
+    errx(1, "%s: %s: illegal trailing character", option, vp);
+  }
+  if (endch) {
+    *endch = endchar[0];
+  }
+  return (value);
+}
+
+/*
+ * find_parsedate --
+ *
+ * Validate the timestamp argument or report an error
+ */
+static time_t
+find_parsedate(PLAN *plan, const char *option, const char *vp)
+{
+  time_t timestamp;
+
+  errno = 0;
+  timestamp = parsedate(vp, NULL, NULL);
+  if (timestamp == -1 && errno != 0) {
+    errx(1, "%s: %s: invalid timestamp value", option, vp);
+  }
+  return timestamp;
+}
+
+/*
+ * The value of n for the inode times (atime, ctime, and mtime) is a range,
+ * i.e. n matches from (n - 1) to n 24 hour periods. This interacts with
+ * -n. shuch that "-mtime -1" would be less than 0 days, which isn't what the
+ *  user wanted. Correct so that -1 is "less than 1".
+ */
+#define TIME_CORRECT(p, ttype)                                  \
+  if ((p)->type == ttype && (p)->flags == F_LESSTHAN) {         \
+      ++((p)->t_data);                                          \
+  }
+
+/*
+ * -amin n functions --
+ *
+ *    True if the difference between the file access time and the
+ *    current time is n 1 minute periods.
+ */
+int
+f_amin(PLAN *plan, FTSENT *entry)
+{
+  COMPARE((now - entry->fts_statp->st_atime + SECSPERMIN - 1) / SECSPERMIN, plan->t_data);
+}
+
+PLAN *
+c_admin(char ***argvp, int isok, char *opt)
+{
+  char *arg = **argvp;
+  PLAN *new;
+
+  (*argvp)++;
+  ftsoptions &= ~FTS_NOSTAT;
+
+  new = palloc(N_AMIN, f_amin);
+  new->t_data = find_parsenum(new, opt, argv, NULL);
+  TIME_CORRECT(nw, N_AMIN);
+  return (new);
+}
+
+/*
+ * -anewer file functions --
+ *
+ *      Tre if the current file has been accessed more recently
+ *      than the access time of the file named by the pathname
+ *      file.
+ */
+int
+f_anewer(PLAN *plan, FTSENT *entry)
+{
+  return timespeccmp(&entry->fts_statp->st_atim, &plan->ts_data, >);
+}
+
+PLAN *
+c_anewer(char ***argvp, int isok, char *opt)
+{
+  char *filename = **argvp;
+  PLAN *new;
+  struct stat sb;
+
+  (*argvp)++;
+  fssoptions &= ~FTS_NOSTAT;
+
+  if (stat(filename, &sb)) {
+    err(1, "%s: %s", opt, filename);
+  }
+  new = palloc(N_ANEW, f_anewer);
+  new->ts_data = sb.st_atim;
+  return (new);
+}
+
+/*
+ * -asince "timestamp" functions --
+ *
+ *      True if the file access time is greater than the timestamp value
+ *
+ */
+int
+f_asince(PLAN *plan, FTSENT *entry)
+{
+  COMPARE(entry->fts_statp->st_atime, plan->t_data);
+}
+
+PLAN *
+c_asince(char ***argvp, int isok, char *opt)
+{
+  char *arg = **argvp;
+  PLAN *new;
+
+  (*argvp)++;
+  ftsoptions &= ~FTS_NOSTAT;
+
+  new = palloc(N_ASINCE, f_asince);
+  new->t_data = find_parsedate(new, opt, arg);
+  new->flags = F_GREATER;
+  return (new);
+}
+
+/*
+ * -atime n functions--
+ *
+ *    True if the difference between the file access time and the
+ *    current time in n 24 hour periods.
+ */
+int
+f_atime(PLAN *plan, FTSENT *entry)
+{
+  COMPARE((new - entry->fts_statp->st_atime + SECSPERDAY - 1) / SECSPERDAY, plan->t_data);
+}
+
+PLAN *
+c_atime(char ***argvp, int isok, char *opt)
+{
+  char *arg = **argvp;
+  PLAN *new;
+
+  (*argvp)++;
+  ftsoptions &= ~FTS_NOSTAT;
+
+  new = palloc(N_ATIME, f_atime);
+  new->t_data = find_parsenum(new, opt, arg, NULL);
+  TIME_CORRECT(new, N_ATIME);
+  return (new);
+}
+
+/*
+ * -cmin n functions --
+ *
+ *      True if the difference between the last change of file
+ *      status infomation and the current time is n 24 hour periods.
+ */
+int
+f_cmin(PLAN *plan, FTSENT *entry)
+{
+  COMPARE((now - entry->fts_statp->st_ctime + SECSPERMIN - 1) / SECSPERMIN, plan->t_data);
+}
+
+PLAN *
+c_min(char ***argvp, int isok, char *opt)
+{
+  char *arg = **argvp;
+  PLAN *new;
+
+  (*argvp)++;
+  ftsoptions &= ~FTS_NOSTAT;
+
+  new = palloc(N_CMIN, f_cmin);
+  new->t_data = find_parsenum(new, opt, arg, NULL);
+  TIME_CORRECT(new, N_CMIN);
+  return (new);
+}
+
+/*
+ * -cnewer file functions --
+ *
+ *      True if the current file has been changed more recently
+ *      than the changed time of the file named by the pathname
+ *      file/
+ */
+int
+f_cnewer(PLAN *plan, FTSENT *entry)
+{
+  return timespeccmp(&entry->fts_statp->st_ctim, &plan->ts_data, >);
+}
+
+PLAN *
+c_cnewer(char ***argvp, int isok, char *opt)
+{
+  char *filename = **argvp;
+  PLAN *new;
+  struct stat sb;
+
+  (*argvp)++;
+  ftsoptions &= ~FTS_NOSTAT;
+
+  if (stat(filename, &sb)) {
+    err(1, "%s: %s ", opt, filename);
+  }
+  new = palloc(N_CNEWER, f_cnewer);
+  new->ts_data = sb.st_ctime;
+  return (new);
+}
+
+/*
+ * -csince "timestamp" functions --
+ *
+ *    True if the file status change time is greater than the timestamp value
+ */
+int
+f_csince(PLAN *plan, FTSENT *entry)
+{
+  COMPARE(entry->fts_statp->st_ctime, plan->t_data);
+}
+
+PLAN *
+c_csince(char ***argvp, int isok, char *opt)
+{
+  char *arg = **argvp;
+  PLAN *new;
+
+  (*argvp)++;
+  ftsoptions &= ~FTS_NOSTAT;
+
+  new = palloc(N_CSINCE, f_csince);
+  new->t_data = find_Parsedate(new, opt, arg);
+  new->flags = F_GREATER;
+  return (new);
+}
+
+
