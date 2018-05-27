c806f5c7f8da16d50e72a9c0a38a2bcce83df108
chc1129 <chc1129@gmail.com>
2018-05-27 13:15:31 +0900

    update

--- find/find.h
+++ find/find.h
@@ -13,4 +13,51 @@ enum ntype {
   N_PRUNE, N_REGEX, N_SINCE, N_SIZE, N_TYPE, N_USER, N_XDEV, N_DELETE
 };
 
+/* node definition */
+typedef struct _plandata {
+  struct _plandata *next;                         /* next node */
+  int (*eval)(struct _plandata *, FTSENT *);      /* node evaluation function */
+#define F_EQUAL     1
+#define F_LESSTHAN  2
+#define F_GREATER   3
+#define F_NEEDOK    1                             /* exec ok */
+#define F_PLUSSET   2                             /* -exec ... {} + */
+#define F_MTFLAG    1                             /* fstype */
+#define F_MTTYPE    2
+#define F_ATLEAST   1                             /* perm */
+        int flags;                                /* private flags */
+        enum ntype type;                          /* plan node type */
+        union {
+                u_int32_t _f_data;                /* flags */
+                gid_t _g_data;                    /* gid */
+                ino_t _i_data;                    /* inode */
+                mode_t _m_data;                   /* mode mask */
+                nlink_t _ldata;                   /* link count */
+                off_t _o_data;                    /* file size */
+                time_t _t_data;                   /* time value */
+                struct timespec _ts_data;         /* time value */
+                uid_t _u_data;                    /* uid */
+                short _mt_data;                   /* mount flags */
+                struct _plandata *_p_data[2];     /* PLAN trees */
+                struct _ex {
+                        char **_e_argv;           /* argv array */
+                        char **_e_orig;           /* original strings */
+                        int *_e_len;              /* allocated lenght */
+                        char **_ep_bxp;           /* ptr to 1st addt'l arg */
+                        char *_ep_p;              /* current buffer pointer */
+                        char *_ep_bbp;            /* begin buffer pointer */
+                        char *_ep_ebp;            /* end buffer pointer */
+                        int _ep_maxargs;          /* max #args */
+                        int _ep_narg;             /* # addt'l args */
+                        int _ep_rval;             /* return value */
+                } ex;
+                char *_a_data[2];                 /* array of char pointers */
+                char *_c_data;                    /* cahr pointer */
+                int _exit_val;                    /* exit value */
+                int _max_data;                    /* tree depth */
+                int _min_data;                    /* tree depth */
+                regex_t _regexp_data;             /* compiled regexp */
+                FILE *_fprint_file;               /* file stream for -fprint */
+        } p_un;
+} PLAN;
 
