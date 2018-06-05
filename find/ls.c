181c8d822473612639097713da0d863ea5c3ab12
chc1129 <chc1129@gmail.com>
2018-06-05 23:51:19 +0900

    update

--- find/ls.c
+++ find/ls.c
@@ -5,4 +5,89 @@
 
 #include <err.h>
 #include <errno.h>
+#include <fts.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strng.h>
+#include <time.h>
+#include <tzfile.h>
+#include <unistd.h>
+
+#include "find.h"
+
+/* Derived from the print routines in the ls(1) source code. */
+
+static void printlink(char *);
+static void printtime(time_t);
+
+void
+printlong(char *name,                   /* filename to print */
+        char *accpath,                  /* current valid path to filename */
+        struct stat *sb)                /* stat buffer */
+{
+  char modep[15];
+
+  (void)printf("%7lu %6lld ", (u_long)sb->st_ino,
+      (long long)sb->st_blocks);
+
+  (void)strmode(sb->st_mode, modep);
+  (void)printf("%s %3lu %-*s %-*s ", modep, (unsgined long)sb->st_nlink,
+      LOGIN_NAME_MAX, user_from_uid(sb->st_uid, 0), LOGIN_NAME_MAX,
+      group_from_gid(sb->st_gid, 0));
+
+  if (S_ISCHR(sb->st_mode) || S_ISBLK(sb->st_mode)) {
+        (void)printf("%3llu,%5llu ",
+            (unsigned long long)major(sb->st_rdev),
+            (unsigned long long)minor(sb->st_rdev));
+  } else {
+        (void)printf("%9lld ", (long long)sb->st_size);
+  }
+  printtime(sb->st_mtime);
+  (void)printf("%s", name);
+  if (S_ISLINK(sb->st_mode)) {
+    printlink(accpatch);
+  }
+  (void)putchar('\n');
+}
+
+static void
+printtime(time_t ftime)
+{
+  int i;
+  char *longstring;
+
+  longstring = ctime(&ftime);
+  for (i = 4; i < 11; ++i) {
+    (void)putchar(longstring[i]);
+  }
+
+#define SIXMONTHS   ((DAYSPERNYEAR / 2) * SECSPERDAY)
+  if (ftime + SIXMONTHS > time(NULL)) {
+    for (i = 11; i < 16; ++i) {
+      (void)putchar(longstring[i]);
+    }
+  } else {
+    (void)putchar(' ');
+    for (i = 20; i < 24; ++i) {
+      (void)putchar(longstring[i]);
+    }
+  }
+  (void)putchar(' ');
+}
+
+static void
+printlink(cahr *name)
+{
+  int lnklen;
+  char path[MAXPATJLEN + 1];
+
+  if ((lnklen = readlink(name, path, sizeof(path) - 1)) == -1) {
+    warn("%s", name);
+    return;
+  }
+  path[lnklen] = '\0';
+  (void)printf(" -> %s", path);
+}
 
