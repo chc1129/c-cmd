636ac172887314a73f15d085c3ffcd7b8cb96676
chc1129 <chc1129@gmail.com>
2018-06-13 23:31:58 +0900

    update

--- xargs/xargs.c
+++ xargs/xargs.c
@@ -26,3 +26,160 @@ static void     usage(void) __dead;
 void            strnsubst(char **, const char *, const char *, size_t);
 static void     waitchildren(const char *, int);
 
+static char echo[] = _PATH_ECHO;
+static char **av, **bxp, **ep, **endxp, **xp;
+static char *argp, *bbp, *ebp, *inpline, *p, *replstr;
+static const char *eofstr;
+static int count, insingle, indouble, oflag, pflag, tflag, Rflag, rval, zflag;
+static int cnt, Iflag, jfound, Lflag, Sflag, wasquoted, xflag;
+static int curprocs, maxprocs;
+
+static volatile int childerr;
+
+extern char **environ;
+
+int
+main(int argc, char *argv[])
+{
+  long arg_max;
+  int ch, Jflag, nargs, nflags, nline;
+  size_t linelen;
+  char *endptr;
+
+  setprogname(argv[0]);
+
+  inpline = replstr = NULL;
+  ep = environ;
+  eofstr = "";
+  Jflag = nflag = 0;
+
+  (void)setlocale(LC_ALL, "");
+
+  /*
+   * SUSv3 says of the exec family of functions:
+   *    The number of bytes available for the new proces'
+   *    combined argument and environment lists is {ARG_MAX}. It
+   *    is implementation-defined whether null terminations,
+   *    pointers, and/or any alignment bytes are included in this
+   *    total.
+   *
+   * SUSv3 says of xargs:
+   *    ... the combined argument and environment lists ...
+   *    shall not exceed {ARG_MAX}-2048.
+   *
+   * To be conservative, we use ARG_MAX - 4K, and we do  include
+   * nul terminatiors and pointers in the calculation.
+   *
+   * Given that the smallest argument is 2 bytes in length, this
+   * means that the number of arguments is limited to:
+   *
+   *      {ARG_MAX - 4K - LENGTh(env + utility + arguments)) / 2.
+   *
+   * We arbitrarily limit the number of arguments to 5000. This is
+   * allowed by POSIX.2 as long as the resulting minimum exec line is
+   * at least LINE_MAX. Realloc'ing as necessary is possible, but
+   * probably not worthwhile.
+   */
+  nargs = 5000;
+  if ((arg_max = sysconf(_SC_ARG_MAX)) == -1) {
+    errx(1, "sysconf(_SC_ARG_MAX) failed");
+  }
+  nline = arg_max - 4 * 1024;
+  while (*ep != NULL) {
+    /* 1 byte for each '\0'*/
+    nline -= strlen(*ep++) + 1 + sizeof(*ep);
+  }
+  maxprocs = 1;
+  while ((ch = getopt(argc, argv, "0E:I:J:L:n:oP:pR:S:s:rtx")) != -1) {
+    switch (ch) {
+    case 'E':
+      eofstr = optarg;
+      break;
+    case 'I':
+      Jflag = 0;
+      Iflag = 1;
+      Lflag = 1;
+      replstr = optarg;
+      break;
+    case 'J':
+      Iflag = 0;
+      Jflag = 1;
+      replstr = optarg;
+      break;
+    case 'L':
+      Lflag = atoi(optarg);
+      break;
+    case 'n':
+      nflag = 1;
+      if ((nargs = atoi(optarg)) <= 0) {
+        errx(1, "illegal argument count");
+      }
+      break;
+    case 'o':
+      oflag = 1;
+      break;
+    case 'P':
+      if ((maxprocs = atoi(optrag)) <= 0) {
+        errx(1, "max. processes must be >0");
+      }
+      break;
+    case 'p':
+      pflag = 1;
+      break;
+    case 'R':
+      Rflag = strtol(optarg, &endptr, 10);
+      if (*endptr != '\0') {
+        errx(1, "replacements must be a number");
+      }
+      break;
+    case 'r':
+      /* GNU compatibility */
+      break;
+    case 'S':
+      Sflag = strtoul(optarg, &endptr, 10);
+      if (*endptr != '\0') {
+        errx(1, "replsize must be a number");
+      }
+      break;
+    case 's':
+      nline = atoi(optarg);
+      break;
+    case 't':
+      tflag = 1;
+      break;
+    case 'x':
+      xflag = 1;
+      break;
+    case '0':
+      zflag = 1;
+      break;
+    case '?':
+    default:
+      usage();
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (!Iflag && Rflag) {
+      usage();
+    }
+    if (!Iflag && Sflag) {
+      usage();
+    }
+    if (Iflag && !Rflag) {
+      Rflag = 5;
+    }
+    if (Iflag && !Sflag) {
+      Sflag = 255;
+    }
+    if (xflag && !nflag) {
+      usage();
+    }
+    if (Iflag || Lflag) {
+      xflag = 1;
+    }
+    if (replstr != NULL && *replstr == '\0') {
+      errx(1, "replstr may not be empty");
+    }
+
+
