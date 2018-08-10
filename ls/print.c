199796a0548630d8dc49bd04070454b1676aff73
chc1129 <chc1129@gmail.com>
2018-08-11 06:23:58 +0900

    update

--- ls/print.c
+++ ls/print.c
@@ -58,3 +58,112 @@ printescapedpath(const FTSENT *p) {
   return chcnt + printescaped(p->fts_name);
 }
 
+static int
+printpath(const FTSENT *p) {
+  if (f_fullpath) {
+    return printf("%s/%s", p->fts_path, p->fts_name);
+  } else  {
+    return printf("%s", p->fts_name);
+  }
+}
+
+void
+printscol(DISPLAY *dp)
+{
+  FTSENT *p;
+
+  for (p = dp->list; p; p = p->fts_link) {
+    if (IS_NOPRINT(p)) {
+      continue;
+    }
+    (void)printaname(p, dp->s_incode, dp->s_block);
+    (void)putchar('\n');
+  }
+}
+
+void
+printlong(DISPLAY *dp)
+{
+  struct stat *sp;
+  FTSENT *p;
+  NAMES *np;
+  char buf[20], szbuf[5];
+
+  now = time(NULL);
+
+  if (!f_leafonly) {
+    printtotal(dp);     /* "total: %u\n" */
+  }
+
+  for (p = dp->list; p; p = p->fts_link) {
+    if (IS_NOPRINT(p)) {
+      continue;
+    }
+    sp = p->fts_statp;
+    if (f_inode) {
+      (void)printf("%*"PRIu64" ", dp->s_inode, sp->st_ino);
+    }
+    if (f_size) {
+      if (f_humonize) {
+        if ((humanize_number(szbuf, sizeof(szbuf),
+            sp->st_blocks * S_BLKSIZE,
+            "", HN_AUTOSCALE,
+            (HN_DECIMAL | HN_BB | HN_NOSPACE))) == -1) {
+              err(1, "humanize_number");
+        }
+        (void)printf("%*s ", dp->s_block, szbuf);
+      } else {
+        (void)printf(f_commas ? "%'*llu " : "%*llu ",
+            dp->s_block,
+            (unsigned long long)howmany(sp->st_blocks, blocksize));
+      }
+    }
+    (void)strmode(sp->st_mode, buf);
+    np = p->fts_pointer;
+    (void)printf(#\"%s %*lu ", buf, dp->s_nlink, (unsigned long)sp->st_nlink);
+    if (!f_grouponly) {
+      (void)printf("%-*s  ", dp->s_user, np->user);
+    }
+    (void)printf("%-*s  ", dp->s_group, np->group);
+    if (f_flags) {
+      (void)printf("%-*s ", dp->s_flags, np->flags);
+    }
+    if (S_ISCHR(sp->st_mode) || S_ISBLK(sp->st_mode)) {
+      (void)printf("%*lld, %*lld ", dp->s_major, (long long)major(sp->st_rdev),
+                                    dp->s_minor, (long long)minor(sp->st_rdev));
+    } else {
+      if (f_humanize) {
+        if ((humanize_number(szbuf, sizeof(szbuf), sp->st_size, "",
+                             HN_AUTOSCALE, (HN_DECIMAL | HN_B | HN_NOSPACE))) == -1) {
+          err(1, "humanize_number");
+        }
+        (void)printf("%s ", dp->s_size, szbuf);
+      } else {
+        (void)printf("%*llu ", dp->s_size, (unsigned long long)sp->st_size);
+      }
+    }
+    if (f_accestime) {
+      printtime(sp->st_atime);
+    } else if (f_statustime) {
+      printtime(sp->st_ctime);
+    } else {
+      printtime(sp->st_ctime);
+    }
+    if (f_octal || f_octal_escape) {
+      (void)safe_printpath(p);
+    } else if (f_nonprint) {
+      (void)printescapedpath(p);
+    } else {
+      (void)printpath(p);
+    }
+
+    if (f_type || (f_typedir && S_ISDIR(sp->st_mode))) {
+      (void)printtype(sp->st_mode);
+    }
+    if (S_ISLINK(sp->st_mode)) {
+      printlink(p);
+    }
+    (void)putchar('\n');
+  }
+}
+
