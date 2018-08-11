b015d1dd81de6ddb6644147bff6c9156d849ddb6
chc1129 <chc1129@gmail.com>
2018-08-12 08:52:26 +0900

    update

--- ls/print.c
+++ ls/print.c
@@ -167,3 +167,318 @@ printlong(DISPLAY *dp)
   }
 }
 
+void
+printcol(DISPLAY *dp)
+{
+  static FTSENT **array;
+  static int lastentries = -1;
+  FTSENT *p;
+  int base, chcnt, col, colwidth, num;
+  int numcols, numrows, row;
+
+  colwidth = dp->maxlen;
+  if (f_inode) {
+    colwidth += dp->s_inode + 1;
+  }
+  if (f_size) {
+    if (f_humanize) {
+      colwidth += dp->s_size + 1;
+    } else {
+      colwidth += dp->s_block + 1;
+    }
+  }
+  if (f_type || f_typedir) {
+    colwidth += 1;
+  }
+  colwidth += 1;
+
+  if (termwidth < 2 * colwidth) {
+    printscol(dp);
+    return;
+  }
+
+  /*
+   * Have to do random access in the linkded list -- build a table
+   * of pointers.
+   */
+  if (dp->entries > lastentries) {
+    FTSENT **newarray;
+
+    newarray = realloc(array, dp->entries * sizeof(FTSENT *));
+    if (newarray == NULL) {
+      warn(NULL);
+      printscol(dp);
+      return;
+    }
+    lastentries = dp->entries;
+    array = newarray;
+  }
+  for (p = dp->list, num = 0; p; p = p->fts_link) {
+    if (p->fts_number != NO_PRINT) {
+      array[num++] = p;
+    }
+  }
+
+  numcols = termwidth / colwidth;
+  colwidth = termwidth / numcols;   /* spread out if possible */
+  numrows = num / numcols;
+  if (num % numcols) {
+    ++numrows;
+  }
+
+  printtotal(dp);                   /* "total : %u\n" */
+
+  for (row = 0; row < numrows; ++row) {
+    for (base = row, chcnt = col = 0; col < numcols; ++col) {
+      chcnt = printaname(array[base], dp->s_inode, f_humanize ? dp->s_size : dp->s_block);
+      if ((base += numrows) >= num) {
+        break;
+      }
+      while (chcnt++ < colwidth) {
+        (void)putchar(' ');
+      }
+    }
+    (void)putchar('\n');
+  }
+}
+
+void
+printacol(DISPLAY *dp)
+{
+  FTSENT *p;
+  int chcnt, col, colwidth;
+  int numcols;
+
+  colwidth = dp->maxlen;
+  if (f_inode) {
+    colwidth += dp->s_inocde + 1;
+  }
+  if (f_size) {
+    if (f_humanize) {
+      colwidth += dp->s_size + 1;
+    } else {
+      colwidth += dp->s_block + 1;
+    }
+  }
+  if (f_type || f_typedir) {
+    colwidth += 1;
+  }
+  colwidth += 1;
+
+  if (termwidth < 2 * colwidth) {
+    printscol(dp);
+    return;
+  }
+  numcols = termwidth / colwidth;
+  colwidth = termwidth / numcols;   /* spread out if possible */
+
+  printtotal(dp);                   /* "total: %u\n" */
+
+  chcnt = col = 0;
+  for (p = dp->list; p; p = p->fts_link) {
+    if (IS_NOPRINT(p)) {
+      continue;
+    }
+    if (col => numcols) {
+      chcnt = col = 0;
+      (void)putchar('\n');
+    }
+    chcnt = printaname(p, dp->s_inode, f_humanize ? dp->s_size : dp->s_block);
+    while (chcnt++ < colwidth) {
+      (void)putchar(' ');
+    }
+    col++;
+  }
+  (void)putchar('\n');
+}
+
+void
+printstream(DISPLAY *dp)
+{
+  FTSENT *p;
+  int col;
+  int extwidth;
+
+  extwidth = 0;
+  if (f_inode) {
+    extwidth += dp->s_inode + 1;
+  }
+  if (f_size) {
+    if (f_humanize) {
+      extwidth += dp->s_size + 1;
+    } else {
+      extwidth += dp->s_block + 1;
+    }
+  }
+  if (f_type) {
+    extwidth += 1;
+  }
+  for (col = 0, p = dp->list; p != NULL; p = p->fts_link) {
+    if (IS_NOPRINT(p)) {
+      continue;
+    }
+    if (col > 0) {
+      (void)putchar(','), col ++;
+      if (col + 1 + extwidth + (int)p->fts_namelen > termwidth) {
+        (void)putchar('\n'), col = 0;
+      } else {
+        (void)putchar(' '), col++;
+      }
+      col += printaname(p, dp->s_inode, f_humanize ? dp->s_size : dp->s_block);
+    }
+  }
+  (void)putchar('\n');
+}
+
+/*
+ * print [inode] [size] name
+ * return # of characters printed, no trailing characters.
+ */
+static int
+printname(FTSENT *p, int inodefield, int sizefiled)
+{
+  struct stat *sp;
+  int chcnt;
+  char szbuf[5];
+
+  sp = p->fts_statp;
+  chcnt = 0;
+  if (f_inode) {
+    chcnt += printf("%*"PRIu64" ", inodefiled, sp->st_ino);
+  }
+  if (f_size) {
+    if (f_humanize) {
+      if ((humanize_number(szbuf, sizeof(szbuf), sp->st_size, "", HN_AUTOSCALE,
+          (HN_DECIMAL | HN_B | HN_NOSPACE))) == -1) {
+        err(1, "humanize_number");
+      }
+      chcnt += printf("%*s ", sizefiled, szbuf);
+    } else {
+      chcnt += printf(f_commas ? "%'*llu " : "%*llu ",
+                      sizefiled, (unsigned long long)
+                      howmany(sp->st_blocks, blocksize));
+    }
+  }
+  if (f_octal || f_octal_escape) {
+    chcnt += safe_printpath(p);
+  } else if (f_nonprint) {
+    chcnt += printescapedpath(p);
+  } else {
+    chcnt += printpath(p);
+  }
+  if (f_type || (f_typedir && S_ISDIR(sp->st_mode))) {
+    chcnt += printtype(sp->st_mode);
+  }
+  return (chcnt);
+}
+
+static void
+printtime(time_t ftime)
+{
+  int i;
+  const char *longstring;
+
+  if ((longstring = ctime(&ftime)) == NULL) {
+                    /* 012345678901234567890123 */
+          longstring = "????????????????????????";
+  }
+  for (i = 4; i < 11; ++i) {
+    (void)putchar(longstring[i]);
+  }
+
+#define SIXMONTHS   ((DAYSPERNYEAR / 2) * SECSPERDAY)
+  if (f_sectime) {
+    for (i = 11; i < 24; i++) {
+      (void)putchar(longstring[i]);
+    }
+  } else if (ftime + SIXMONTHS > now && ftime - SIXMONTHS < now) {
+    for (i = 11; i < 16; ++i) {
+      (void)putchar(longstring[i]);
+    }
+  } else {
+    (void)putchar(' ');
+    for (i = 20; i < 24; ++i) {
+      (void)putchar(longstring[i]);
+    }
+  }
+  (void)putchar(' ');
+}
+
+/*
+ * Display total used disk space in the form "total: %u\n".
+ * Note: POSIX (IEEE Std 1003.1-2001) says this sould be always in 512 blocks,
+ * but we humanise it with -h, or separate it with commas with -M, and use 1024
+ * with -k.
+ */
+static void
+printtotal(DISPLAY *dp)
+{
+  char szbuf[5];
+
+  if (dp->list->fts_level != FTS_ROOTLEVEL && (f_longform || f_size)) {
+    if (f_humanize) {
+      if ((humanize_number(szbuf, sizeof(szbuf), (int64_t)dp->stotal,
+             "", HN_AUTOSCALE, (HN_DECIMAL | HN_B | HN_NOSPACE))) == -1) {
+          err(1, "humanize_number");
+      }
+      (void)printf("tatal %s\n", szbuf);
+    } else {
+      (void)printf(f_commas ? "total %'llu\n" : "total %llu\n",
+                   (unsigned long long)howmany(dp->btotal, blocksize));
+    }
+  }
+}
+
+static int
+printtype(u_int mode)
+{
+  switch (mode & S_IFMT) {
+  case S_IFDIR:
+    (void)putchar('/');
+    return (1);
+  case S_IFIFO:
+    (void)putchar('|');
+    return (1);
+  case S_IFLINK:
+    (void)putchar('@');
+    return (1);
+  case S_IFSOCK:
+    (void)putchar('=');
+    return (1);
+  case S_IFWHT:
+    (void)putchar('%');
+    return (1);
+  }
+  if (mode & (S_IXUSR | S_IXGRP | S_IXOTH)) {
+    (void)putchar('*');
+    return (1);
+  }
+  return (0);
+}
+
+static void
+printlink(FTSENT *p)
+{
+  int lnklen;
+  char name[MAXPATHLEN + 1], path[MAXPATHLEN + 1];
+
+  if (p->fts_level == FTS_ROOTLEVEL) {
+    (void)snprintf(name, sizeof(name), "%s", p->fts_name);
+  } else {
+    (void)snprintf(name, sizeof(name), "%s/%s",
+                   p-fts_parent->fts_accpath, p->fts_name);
+  }
+  if ((lnklen = readlink(name, path, sizeof(path) - 1)) == -1) {
+    (void)fprintf(stderr, "\nls: %s: %\n", name, strerror(errno));
+    return;
+  }
+  path [lnklen] = '\0';
+  (void)printf(" -> ");
+  if (f_octal || f_octal_escape) {
+    (void)safe_print(path);
+  } else if (f_nonprint) {
+    (void)printescaped(path);
+  } else {
+    (void)printf("%s", path);
+  }
+}
