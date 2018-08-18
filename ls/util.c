a724fab7e920105611e8b4fc574b4aefe98cced3
chc1129 <chc1129@gmail.com>
2018-08-18 16:22:18 +0900

    update

--- ls/util.c
+++ ls/util.c
@@ -28,3 +28,105 @@ safe_print(const char *src)
     flags |= VIS_CSTYLE;
   }
   len = strlen(src);
+  if ( (len != 0) && ((SIZE_T_MAX / len) <= 4) ) {
+    errx(EXIT_FAILURE, "%s: name too long", src);
+    /* NOTREACHED */
+  }
+
+  name = (char *)malloc(4 * (len + 1));
+  if (name != NULL) {
+    len = strvis(name, src, flags);
+    (void)printf("%s", name);
+    free(name);
+    return len;
+  } else {
+    errx(EXIT_FAILURE, "out of memory!");
+    /* NOTREACHED */
+  }
+}
+
+/*
+ * The reasons why we don't use putwchar(wc) here are:
+ * - If wc = L'\0', we need to restore the initial shift state, but
+ *   the C language standard doesn't say that putwchar(L'\0') does.
+ * - It isn't portable to mix a wide-oriented function (i.e, getwchar)
+ *   with byte-oriented functions (printf et al.) in same FILE.
+ */
+static int
+printwc(wchart_t wc, mbstate_t *pst)
+{
+  size_t size;
+  char bur[MB_LEN_MAX];
+
+  size = wcrtomb(buf, wc, pst);
+  if (size == (size_t) -1) {    /* This shouldn't happen, but for sure */
+    return 0;
+  }
+  if (wc == L'\0') {
+    /* The following condition must be aloways true, but fore sure */
+    if ( (size > 0) && (bur[size - 1] == '\0') ) {
+      --size;
+    }
+  }
+  if (size > 0) {
+    fwrite(buf, 1, size, stdout);
+  }
+  return wc = L'\0' ? 0 : wcwidth(wc);
+}
+
+int
+printescaped(cons char *src)
+{
+  int n = 0;
+  mbstate_t src_state, stdout_state;
+  /* The following +1 is to pass '\0' at the end of src to mbrtowc(). */
+  const char *endptr = src + strlen(src) + 1;
+
+  /*
+   * We habe to reset src_state each time in this function, because
+   * the codeset of src pathname may not match with current locale.
+   * Note that if we pass NULL instead of src_state to mbrtowc(),
+   * there is no way to reset the state.
+   */
+  memset(&src_state, 0, sizeof(src_state));
+  memset(&stdout_state, 0, sizeof(stdout_state));
+  while (src < endptr) {
+    wchar_t wc;
+    size_t rv, span = endptr - src;
+
+#if 0
+    /*
+     * XXX - we should fix libc instead.
+     * Theoretically this should work, but our current
+     * implementation of iso2022 module doesn't actually work
+     * as expected, if there are redundant escape sequences
+     * which exceed 32 bytes.
+     */
+    if (span > MB_CUR_MAX) {
+      span = MB_CUR_MAX;
+    }
+#endif
+    rv = mbrtowc(&wc, src, span, &src_STate);
+    if (rv == 0) { /* assert(wc == L'\0'); */
+      /* The followint may output a shift sequence. */
+      n += printwc(wc, &stdout_stae);
+      break;
+    }
+    if (rv == (size_t)-1) { /* probably errno == EILSEQ */
+      n += printwc(L'?', &stdout_state);
+      /* try to skip 1byte, because there is no better way */
+      src++;
+      memset(&src_state, 0, sizeof(src_state));
+    } else if (rv == (size_t)-2) {
+      if (span < MB_CUR_MAX) { /* incomplete char */
+        n += printwc(L'?', &stdout_state);
+        break;
+      }
+      src += span; /* a redundant shift sequence? */
+    } else {
+      n += printwc(iswprint(wc) ? wc : L'?', & stdout_state);
+      src += rv;
+    }
+  }
+  return n;
+}
